276610c58f8b9a45e28680c8a797d40ad748bc3f
rosiachmad <ealbri4@my.wgu.edu>
2018-04-03 20:37:15 +0700

    Add files via upload

---
+++ resiku.js
@@ -0,0 +1,3 @@
+function init_widget_cekresicom(v,width,height) {
+document.getElementById("cekresicom_id").innerHTML='<iframe src="https://cekresi.com/widget/'+v+'?iframe=1&h='+window.location.host+'" width="'+width+'" height="'+height+'" style="border:0px;" border="0" frameborder="0"></iframe>';
+}
